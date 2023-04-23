d501210b9de06c2675fb0f3e4f34bbc2feca5019
Kirill22 <anikinkirill22@gmail.com>
2023-04-24 01:55:38 +0300

    FE: wip

---
+++ web/components/app-header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header className="header">My custom header</header>
+</template>
+
+<style scoped lang="scss">
+@import "../variables.scss";
+
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 66px;
+
+  background-color: $primary-color;
+  color: $white;
+}
+</style>
--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,4 +1,24 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+import vuetify from "vite-plugin-vuetify";
+import { createResolver } from "@nuxt/kit";
 
-})
+const { resolve } = createResolver(import.meta.url);
+
+export default defineNuxtConfig({
+  css: [
+    "vuetify/lib/styles/main.sass",
+    "@mdi/font/css/materialdesignicons.min.css",
+  ],
+  pages: true,
+  build: {
+    transpile: ["vuetify"],
+  },
+  hooks: {
+    "vite:extendConfig": (config) => {
+      config?.plugins?.push(
+        vuetify({
+          styles: { configFile: resolve("./settings.scss") },
+        })
+      );
+    },
+  },
+});
